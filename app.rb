763ce19a643ed0e1445571a582d730c6f1aeb112
Roberto Samia <bojosteph@gmail.com>
2019-02-11 01:15:52 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,13 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    get '/' do
+      erb :'pirates/index'
+    end
+    
+    get '/new' do
+      erb :'pirates/new'
+    end
 
   end
 end
